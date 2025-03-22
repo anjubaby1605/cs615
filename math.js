2619ccc91e0fad6b0826703fd0c37a9cbacf7999
Anju Baby <anjubaby1605@gmail.com>
2025-03-22 01:54:41 +0000

    Assignment 5

---
+++ math.js
@@ -0,0 +1,7 @@
+module.exports.counter = function(arr){
+    return 'There are ' + arr.length + ' elements in this array.';
+   }
+   module.export.adder = function(a,b){
+      return `The sum of the 2 numbers is ${a+b}`;
+   }
+   module.expoert.pi = 3.1415
